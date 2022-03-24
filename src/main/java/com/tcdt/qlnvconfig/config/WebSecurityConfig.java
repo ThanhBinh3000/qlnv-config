e25eab7b84232eeb396b1ec87c2577fd8a470889
phananhtk <phananh1304@gmail.com>
2022-03-24 16:27:52 +0700

    fig

--- src/main/java/com/tcdt/qlnvconfig/config/WebSecurityConfig.java
+++ src/main/java/com/tcdt/qlnvconfig/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.tcdt.qlnvconfig.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,8 +10,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/actuator/**").permitAll().and().authorizeRequests()
+		http.csrf().disable().authorizeRequests().antMatchers("/**", "/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll().and().authorizeRequests()
 				.anyRequest().authenticated().and().httpBasic();
 	}
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+    }
 
 }
